db2a47e7c16d82d08d1d561d32774c3c28d97656
samaugust <sam.august@gmail.com>
2016-03-08 23:26:22 -0700

    Front-end CSS; Revise Games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,58 +1,46 @@
 class GamesController < ApplicationController
-	before_action :find_game, only: [:show, :edit, :update, :destroy]
+  before_action :find_game, only: [:show, :edit, :update, :destroy]
 
-	def index
-		if Game.all.count >= 1
-			@games = Game.all
-			@decks = Game.select('deck').all.order("created_at DESC")
-		end
-	end
+  def index
+    if Game.all.count >= 1
+      @games = Game.all.order("created_at DESC")
+      @decks = @games.map { |x| x.deck }
+    end
+  end
 
-	def new
-		@game = Game.new 
-		@game.update_attribute(:deck, (2..14).flat_map { |rank| ("a".."d").map { |suit| (rank.to_s + suit) } }.shuffle)
-		redirect_to root_path
-	end
+  def create
+    @game = Game.new({deck: ("2".."14").flat_map { |rank| ("a".."d").map { |suit| (rank + suit) } }.shuffle}, params[:id])
+    @game.save
+    Game.first.destroy if Game.all.count > 1
+    redirect_to root_path
+  end
 
-	def create
-   	@game = Game.new(params[:id])
-   	@game.update_attribute(:deck, (2..14).flat_map { |rank| ("a".."d").map { |suit| (rank.to_s + suit) } }.shuffle)
-    if @game.save
-			redirect_to @game
-		end
-	end
+  def give_me_chips
+    current_player.chips.nil? ? current_player.update_attribute(:chips, 500) : current_player.update_attribute(:chips, current_player.chips + 500)
+    redirect_to root_url
+  end
 
-	def give_me_chips
-		@user = current_user
-		@user.chips.nil? ? @user.update_attribute(:chips, 500) : @user.update_attribute(:chips, @user.chips + 500)
-		redirect_to root_url
-	end
+  def edit
+  end
 
-	def output
-		
-	end
+  def update
+  end
 
-	def edit
-	end
+  def show
+    @deck = @game.deck
+  end
 
-	def update
-	end
+  def destroy
+    @game.destroy
+    redirect_to root_path
+  end
 
-	def show
-		@deck = @game.deck
-	end
+  private
+    def game_params
+      params.require(:game).permit(:deck, :pot, :community_cards)
+    end
 
-	def destroy
-		@game.destroy
-		redirect_to root_path
-	end
-
-	private
-		def deck_params
-			params.require(:game).permit(:deck, :pot, :community_cards)
-		end
-
-		def find_game
-			@game = Game.find(params[:id])
-		end
+    def find_game
+      @game = Game.find(params[:id])
+    end
 end
